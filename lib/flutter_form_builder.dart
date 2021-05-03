57a806cd66f4a4c00f94bdbc09dd98561d6e8254
Srishan Shakya <csunshakya@gmail.com>
2021-05-03 18:32:43 +0545

    Add FormBuilderSearchableDropdown ref:lferrerad/flutter_form_builder.git

--- lib/flutter_form_builder.dart
+++ lib/flutter_form_builder.dart
@@ -1,5 +1,7 @@
 library flutter_form_builder;
 
+export 'package:dropdown_search/dropdown_search.dart' hide ErrorBuilder;
+
 export './localization/form_builder_localizations.dart';
 export './src/fields/form_builder_checkbox.dart';
 export './src/fields/form_builder_checkbox_group.dart';
@@ -10,6 +12,7 @@ export './src/fields/form_builder_dropdown.dart';
 export './src/fields/form_builder_filter_chips.dart';
 export './src/fields/form_builder_radio_group.dart';
 export './src/fields/form_builder_range_slider.dart';
+export './src/fields/form_builder_searchable_dropdown.dart';
 export './src/fields/form_builder_segmented_control.dart';
 export './src/fields/form_builder_slider.dart';
 export './src/fields/form_builder_switch.dart';
